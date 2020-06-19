68e0db109cf46eac2f7049c94c7f6a586c0fe185
Vinicius Rodrigues Gimenes <vinicius11_gimenes@hotmail.com>
2020-06-19 12:22:53 -0300

    hotfix 3

--- features/support/env.rb
+++ features/support/env.rb
@@ -34,5 +34,5 @@ end
 Capybara.configure do |config|
   config.default_driver = @driver
   config.app_host = CONFIG['url']
-  config.default_max_wait_time = 7
+  config.default_max_wait_time = 20
 end
