59e9acc5ae782ea5d0f3c2c46001b82e7554ebc4
Vinicius Rodrigues Gimenes <vinicius11_gimenes@hotmail.com>
2020-06-19 11:56:20 -0300

    hotfix

--- features/support/env.rb
+++ features/support/env.rb
@@ -18,13 +18,13 @@ when 'chrome'
   @driver = :selenium_chrome
 when 'headless'
   Capybara.register_driver :selenium_chrome_headless do |app|
-    chrome_options = Selenium::WebDriver::Chrome::Options.new.tap do |opts|
+    chrome_options =  Selenium::WebDriver::Chrome::Options.new.tap do |opts|
       opts.add_argument "--headless"
       opts.add_argument "--disable-gpu"
       opts.add_argument "--no-sandbox"
       opts.add_argument "--disable-site-isolation-trials"
     end
-    Capybara::Selenium::Driver.new(app, browser: :chrome, options: chrome_options )
+    Capybara::Selenium::Driver.new(app, browser: :chrome, options: chrome_options)
   end
   @driver = :selenium_chrome_headless
 else
