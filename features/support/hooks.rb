190f17774f4fdde73bf210e9366b14734bdf7fa4
Vinicius Rodrigues Gimenes <vinicius11_gimenes@hotmail.com>
2020-06-19 11:16:52 -0300

    hooks hotfix

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -15,6 +15,6 @@ end
 
 After do
   temp_shot = page.save_screenshot('log/temp_shot.png')
-  screenshot = Base64.encode64(File.open(temp_shot).read)
+  screenshot = Base64.encode64(File.open(temp_shot, "rb").read)
   embed(screenshot, 'image/png', 'Screenshot')
 end
